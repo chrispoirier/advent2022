1b313e3efc5363e8d7ca645ddc7dcf6cf6fe3e32
Chris Poirier <chris.poirier@gmail.com>
2022-12-04 21:59:45 -0700

    day2 part2

---
+++ day2/part2/target/debug/deps/part1.d
@@ -0,0 +1,5 @@
+P:\Documents\advent2022\day2\part2\target\debug\deps\part1.exe: src\main.rs
+
+P:\Documents\advent2022\day2\part2\target\debug\deps\part1.d: src\main.rs
+
+src\main.rs:
---
+++ day2/part2/target/debug/part2.d
@@ -0,0 +1,1 @@
+P:\Documents\advent2022\day2\part2\target\debug\part2.exe: P:\Documents\advent2022\day2\part2\src\main.rs
