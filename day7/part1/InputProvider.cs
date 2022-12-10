3956802c0b1a1488e45107d51100e6003172ec02
Chris Poirier <chris.poirier@gmail.com>
2022-12-10 01:05:19 -0700

    day 7 part 1

---
+++ day7/part1/InputProvider.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace part1
+{
+    internal static class InputProvider
+    {
+        internal static async Task<List<string>> GetFileLines(string filename)
+        {
+            return new List<string>(await System.IO.File.ReadAllLinesAsync(filename));
+        }
+    }
+}
