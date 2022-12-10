07b7c49829cb04ea6e0fd9e11f784727bb3691e1
Chris Poirier <chris.poirier@gmail.com>
2022-12-10 01:24:52 -0700

    day 7 part 2

---
+++ day7/part2/File.cs
@@ -0,0 +1,14 @@
+﻿namespace part2
+{
+    class File
+    {
+        public string Label { get; }
+        public int Size { get; }
+
+        public File(string label, int size)
+        {
+            Label = label;
+            Size = size;
+        }
+    }
+}
