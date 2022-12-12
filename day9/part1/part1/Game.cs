25557dc87571cd037fa1d26a9333375ddf4224bd
Chris Poirier <chris.poirier@gmail.com>
2022-12-12 00:33:35 -0700

    day9 part1

---
+++ day9/part1/part1/Game.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace part1
+{
+    internal class Game
+    {
+        internal Rope Rope { get; set; }
+        internal List<Move> Moves { get; set; }
+        internal HashSet<Point> TailVisited { get; set; }
+
+        internal Game(List<string> movesStr) 
+        {
+            Rope = new Rope(Point.Empty, Point.Empty);
+            Moves = new List<Move>();
+            InitMoves(movesStr);
+            TailVisited = new HashSet<Point>() { Point.Empty };
+        }
+
+        private void InitMoves(List<string> movesStr)
+        {
+            foreach(var moveStr in movesStr)
+            {
+                var move = Move.FromString(moveStr);
+                if(move != null) Moves.Add(move);
+            }
+        }
+
+        internal int Run()
+        {
+            foreach(var move in Moves)
+            {
+                var tailVisited = Rope.MoveHead(move);
+                //Console.WriteLine($"H: ({Rope.HeadPos.X},{Rope.HeadPos.Y}) | T: ({Rope.TailPos.X},{Rope.TailPos.Y})");
+                //Console.WriteLine("tv: [" + String.Join(';', tailVisited) + "]");
+                if (tailVisited != null)
+                {
+                    foreach(var visPoint in tailVisited)
+                    {
+                        TailVisited.Add(visPoint);
+                    }
+                }
+                //Console.WriteLine("TV: [" + String.Join(';', TailVisited) + "]\n");
+            }
+            return TailVisited.Count;
+        }
+    }
+}
---
+++ day9/part1/part1/Move.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace part1
+{
+    internal class Move
+    {
+        internal enum MoveDirection
+        {
+            Up,
+            Down,
+            Left,
+            Right
+        }
+
+        internal MoveDirection Direction { get; set; }
+        internal int Count { get; set; }
+
+        internal Move(MoveDirection direction, int count)
+        {
+            Direction = direction;
+            Count = count;
+        }
+
+        internal static Move? FromString(string str)
+        {
+            var exp = new Regex("([UDLR]) (\\d+)");
+            var match = exp.Match(str);
+            if(match != null)
+            {
+                return new Move(CharToDirection(match.Groups[1].Value[0]), Convert.ToInt32(match.Groups[2].Value));
+            }
+            return null;
+        }
+
+        private static MoveDirection CharToDirection(char c)
+        {
+            switch (c)
+            {
+                case 'U': return MoveDirection.Up;
+                case 'D': return MoveDirection.Down;
+                case 'L': return MoveDirection.Left;
+                default: return MoveDirection.Right;
+            }
+        }
+    }
+}
