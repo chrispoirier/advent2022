cf0b01db0cfc4c3492ec34bfbb68865b3919b12f
Chris Poirier <chris.poirier@gmail.com>
2022-12-12 23:09:37 -0700

    day 9 part 2

---
+++ day9/part2/part2/Game.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace part2
+{
+    internal class Game
+    {
+        internal Rope Rope { get; set; }
+        internal List<Move> Moves { get; set; }
+        internal HashSet<Point> TailVisited { get; set; }
+
+        internal Game(List<string> movesStr) 
+        {
+            Rope = new Rope();
+            Moves = new List<Move>();
+            InitMoves(movesStr);
+            TailVisited = new HashSet<Point>() { Point.Empty };
+        }
+
+        private void InitMoves(List<string> movesStr)
+        {
+            foreach(var moveStr in movesStr)
+            {
+                var move = Move.FromString(moveStr);
+                if(move != null) Moves.Add(move);
+            }
+        }
+
+        internal int Run()
+        {
+            foreach(var move in Moves)
+            {
+                var tailVisited = Rope.MoveHead(move);
+                //Rope.PrintKnots();
+                //Console.WriteLine("tv: [" + String.Join(';', tailVisited) + "]");
+                if (tailVisited != null)
+                {
+                    foreach(var visPoint in tailVisited)
+                    {
+                        TailVisited.Add(visPoint);
+                    }
+                }
+                //Console.WriteLine("TV: [" + String.Join(';', TailVisited) + "]\n");
+            }
+            return TailVisited.Count;
+        }
+    }
+}
---
+++ day9/part2/part2/Point.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace part2
+{
+    internal class Point
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public Point(Point source)
+        {
+            X = source.X;
+            Y = source.Y;
+        }
+
+        public static Point Empty
+        {
+            get
+            {
+                return new Point(0, 0);
+            }
+        }
+
+        public override string ToString()
+        {
+            return "(" + X + "," + Y + ")";
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (!(obj is Point) || obj == null) return false;
+            var pt = obj as Point;
+            return X == pt.X && Y == pt.Y;
+        }
+
+        public override int GetHashCode()
+        {
+            return X.GetHashCode() * Y.GetHashCode();
+        }
+    }
+}
