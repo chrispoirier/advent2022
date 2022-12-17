2dfaf29049ba6605f5a7c3b5c8ef5ae23b8c02d5
Chris Poirier <chris.poirier@gmail.com>
2022-12-16 18:26:16 -0700

    day 11 part 1

---
+++ day11/part1/part1.py
@@ -0,0 +1,9 @@
+from game import Game
+
+input = open("input", "r")
+data = input.readlines()
+game = Game.fromData(data)
+#print(str(game))
+game.run(20)
+print(str(game))
+print("Monkey business: " + str(game.monkeyBusiness()))
